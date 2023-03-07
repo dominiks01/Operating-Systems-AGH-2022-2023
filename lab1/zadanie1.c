a01d88cbf895fc871d2453ef65af925c673d8d6a
dominiks01 <dominikszot950@gmail.com>
2023-03-07 16:15:49 +0100

    ex1

--- lab1/zadanie1.c
+++ lab1/zadanie1.c
@@ -28,45 +28,31 @@ void countLinesAndWords(dataBlock* data, char* filename){
 
     // Making command for word counter using snprintf 
     char command [255];
-    int cx; 
-    cx = snprintf(command, 255, "wc --chars --lines --words %s > /tmp/result.txt", filename);
+    int cx = snprintf(command, 255, "wc --chars --lines --words %s > /tmp/result.txt", filename);
 
     system(command);
+    int indexToWrite = -1;
+
+    for(int i = 0; i < data->capacity; ++i)
+        if(data->memory[i] == 0){
+            indexToWrite = i; 
+            break;
+        }
     
-    // Getting number of byts for file content from result of wc command
+    if(indexToWrite == -1)
+        return;
+    
     FILE *fp; 
     fp = fopen("/tmp/result.txt", "r");
 
-    char line[255];
-    fgets(line,255, fp);
+    fseek(fp, 0L, SEEK_END);
+    size_t size = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
 
-    char * result; 
-    result = strtok(line, " ");
-    result = strtok(NULL, " ");
-    result = strtok(NULL, " ");
-
-    size_t size;
-    sscanf(result, "%zu", &size);
-    
     // Reopening content file to write into out memory block  
-    fclose(fp);
-    fp = fopen(filename, "r");
-    data->memory[data->size] = (char*)calloc(sizeof(char*), size);
+    data->memory[indexToWrite] = (char*)calloc(sizeof(char*), size);
 
-    // char* buffer = (char*)calloc(size, sizeof(char));
-    fread(data->memory[data->size], sizeof(char), size, fp);
-
-    //memoryBlock * block = calloc(sizeof(memoryBlock), 1);
-    //block->content =  calloc(sizeof(char*), size+1);
-    // block = calloc(sizeof(memoryBlock), 1);
-
-    // data->memory[data->size] = buffer;
-    //data->memory[data->size] = (char*)calloc(sizeof(char*), size);
-    //strcpy(data->memory[data->size], buffer);
-
-    // strcpy(block->content, buffer);
-    // data->memory[data->size] = block;
-    
+    fread(data->memory[indexToWrite], sizeof(char), size, fp);    
     data->size += 1;
 
     // Removing unnecessary result file
@@ -80,10 +66,19 @@ char* getBlock(dataBlock* data, int index){
     return data->memory[index];
 }
 
-void* deleteBlock(dataBlock* data, int index){
-    data->size -= 1;
+void deleteBlock(dataBlock* data, int index){
+    if(data->memory[index] == NULL)
+        return;
     free(data->memory[index]);
+    data->memory[index] = NULL;
 }
+
+void deleteDataBlock(dataBlock* data){
+    for(int i = 0; i < data->capacity; i++)
+        deleteBlock(data, i);
+    free(data);
+}
+
 
 
 int main(){
@@ -91,9 +86,7 @@ int main(){
     countLinesAndWords(newData, "test.txt");
     char * block = getBlock(newData, 0);
     puts(block);
-
-
-    free(newData);
-    free(block); 
+    deleteBlock(newData, 0);
+    deleteDataBlock(newData);
     return 0;
 }
